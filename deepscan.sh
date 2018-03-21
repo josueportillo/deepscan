d8b8dea6f23a4c107ca6d3f1396a463b88f23c9a
Administrator <josueportillo@gmail.com>
2018-03-21 01:38:23 -0300

    v1.1.2. See README.md

--- deepscan.sh
+++ deepscan.sh
@@ -12,13 +12,28 @@
 #*************************************************************************************************************
 
 #Check if the file is being run as root (or with sudo)
+
 if [ "$(id -u)" != "0" ]; then
 	echo "Must be run as root. Exiting..."
 	exit 1
 fi
+
+#Check if Nmap is installed
+
+echo "Checking to see if Nmap is installed."
+if [ -a nmap ]; then
+        echo "Nmap installed. Good to go."
+else
+        echo "Nmap not installed. Please install Nmap and rerun this program."
+        echo "Quitting..."
+        exit 1
+fi
+
+
 #Selection menu
+
 echo ' '
-echo ******************Deepscan v1.1***********************
+echo ******************Deepscan v1.1.2**********************
 echo ' '
 echo Please select the type of scan:
 echo ' '
@@ -26,6 +41,7 @@ echo [1] Automatic [scans all locally connected networks].
 echo [2] Manually enter target network.
 echo [3] Display README file.
 echo ' '
+echo [q] Quit.
 echo Please enter your selection and press ENTER:
 read option
 
@@ -39,12 +55,12 @@ if [ $option = 1 ]; then
 	start=`date +%s`
 #Gather local networks via the ROUTE command and start scanning for hosts in found networks.
 	route | grep "*" | for networks in $(cut -d " " -f 1); 
-		do echo $networks >> deepscan_tmplist && echo "***Found $networks/24 network segment. Creating folder deepscan/$targetname/$networks for storing results and scanning for live hosts.***" &&  mkdir -p deepscan/$targetname/$networks && echo 'Folder created.'  && echo 'Running scan, please wait...' && nmap -sn $networks/24 | grep 'Nmap scan report' | cut -f 5 -d " " > deepscan/$targetname/$networks/hostlist && echo 'Found hosts:' && cat deepscan/$targetname/$networks/hostlist;
+		do echo $networks >> deepscan_tmplist && echo "***Found $networks/24 network segment. Creating folder $HOME/deepscan/$targetname/$networks for storing results and scanning for live hosts.***" &&  mkdir -p $HOME/deepscan/$targetname/$networks && echo 'Folder created.'  && echo 'Running scan, please wait...' && nmap -sn $networks/24 | grep 'Nmap scan report' | cut -f 5 -d " " > $HOME/deepscan/$targetname/$networks/hostlist && echo 'Found hosts:' && cat $HOME/deepscan/$targetname/$networks/hostlist;
 		done
 #Port scan of found hosts
 	echo '***Scanning individual hosts for open ports, please wait...***';
 	for networks in $(cat deepscan_tmplist);
-		do for host in $(cat deepscan/$targetname/$networks/hostlist);
+		do for host in $(cat $HOME/deepscan/$targetname/$networks/hostlist);
 			do nmap -sV -sS -Pn $host | grep 'open' > deepscan/$targetname/$networks/openports_$host && echo "Open ports for $host:" && cat deepscan/$targetname/$networks/openports_$host; 
 			done;
 		done
@@ -56,6 +72,7 @@ if [ $option = 1 ]; then
 #Calculate script runtime and display it in a user friendly way.
 	runtime=$((end-start))
 	echo Done in $runtime seconds.
+	exit 0
 fi
 
 #Manual scan
@@ -71,13 +88,13 @@ if [ $option = 2 ]; then
 #Start time to calculate runtime.
         start=`date +%s`
 	for networks in $network; 
-                do echo $networks >> deepscan_tmplist && echo "***Using $networks/$netmask network segment. Creating folder deepscan/$targetname/$networks for storing results and scanning for live hosts.***" &&  mkdir -p deepscan/$targetname/$networks && echo 'Folder created.'  && echo 'Running scan, please wait...' && nmap -sn $networks/$netmask | grep 'Nmap scan report' | cut -f 5 -d " " > deepscan/$targetname/$networks/hostlist && echo 'Found hosts:' && cat deepscan/$targetname/$networks/hostlist;
+                do echo $networks >> deepscan_tmplist && echo "***Using $networks/$netmask network segment. Creating folder $HOME/deepscan/$targetname/$networks for storing results and scanning for live hosts.***" &&  mkdir -p $HOME/deepscan/$targetname/$networks && echo 'Folder created.'  && echo 'Running scan, please wait...' && nmap -sn $networks/$netmask | grep 'Nmap scan report' | cut -f 5 -d " " > $HOME/deepscan/$targetname/$networks/hostlist && echo 'Found hosts:' && cat $HOME/deepscan/$targetname/$networks/hostlist;
                 done
 #Port scan of found hosts
         echo '***Scanning individual hosts for open ports, please wait...***';
         for networks in $(cat deepscan_tmplist);
-                do for host in $(cat deepscan/$targetname/$networks/hostlist);
-                        do nmap -sV -sS -Pn $host | grep 'open' > deepscan/$targetname/$networks/openports_$host && echo "Open ports for $host:" && cat deepscan/$targetname/$networks/openports_$host; 
+                do for host in $(cat $HOME/deepscan/$targetname/$networks/hostlist);
+                        do nmap -sV -sS -Pn $host | grep 'open' > $HOME/deepscan/$targetname/$networks/openports_$host && echo "Open ports for $host:" && cat $HOME/deepscan/$targetname/$networks/openports_$host; 
                         done;
                 done
 #Delete temporary files created during the scan
@@ -88,11 +105,19 @@ if [ $option = 2 ]; then
 #Calculate script runtime and display it in a user friendly way.
         runtime=$((end-start))
         echo Done in $runtime seconds.
+	exit 0
 fi
 
 #Show the README.md file
 if [ $option = 3 ]; then
-	less bash/deepscan/README.md
+	less $HOME/bash/deepscan/README.md
+	$HOME/bash/deepscan/deepscan.sh #testing return to main menu
+	#exit 0
+fi
+
+#Quit the program
+if [ $option = q ]; then
+	echo "Bye!"
 	exit 0
 fi
 
